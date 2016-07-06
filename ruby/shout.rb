774dba77d968eada3b1495430d757553519fa071
Connor Ray <connor.ray.cb@gmail.com>
2016-07-06 16:09:57 -0600

    created modules

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,32 @@
-module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+#
+#   def self.yelling_happily(words)
+#     words + "!!!" + " :D"
+#   end
+# end
+#
+# p Shout.yelling_happily("I'm so happy")
+# p Shout.yell_angrily("I'm so angry")
 
-  def self.yelling_happily(words)
+module Shout
+  def yelling_happily(words)
     words + "!!!" + " :D"
   end
 end
 
-p Shout.yelling_happily("I'm so happy")
-p Shout.yell_angrily("I'm so angry")
+class Boy
+  include Shout
+end
+
+class Girl
+  include Shout
+end
+
+boy = Boy.new
+p boy.yelling_happily("Hello")
+
+girl = Girl.new
+p girl.yelling_happily("Hello")
