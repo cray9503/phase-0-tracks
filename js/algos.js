e8d6e0877769bbab96c3d4758b029ecd9b393496
Connor Ray <connor.ray.cb@gmail.com>
2016-07-14 16:14:26 -0600

    Created key-pair function

--- js/algos.js
+++ js/algos.js
@@ -17,5 +17,37 @@ function longest_phrase(array) {
 }
 
 
-(longest_phrase(['apple', 'pear', 'grapes']));
-(longest_phrase(["long phrase","longest phrase","longer phrase"]));
+// (longest_phrase(['apple', 'pear', 'grapes']));
+// (longest_phrase(["long phrase","longest phrase","longer phrase"]));
+
+
+// Takes two objects and checks to see if the objects share at least one key-value pair.
+
+function key_match(object_1, object_2) {
+
+  var object1Keys = Object.keys(object_1);
+  var object2Keys = Object.keys(object_2);
+
+  var allObjectKeys = object1Keys.concat(object2Keys);
+  var hasMatch = false;
+
+  for (var i = 0; i < allObjectKeys.length; i++){
+    var key = allObjectKeys[i];
+
+    var keyInObject1 = (object1Keys.indexOf(key) !== -1);
+    var keyInObject2 = (object2Keys.indexOf(key) !== -1);
+
+    if ( keyInObject1 &&
+         keyInObject2 &&
+         (object_1[key] === object_2[key]) ) {
+
+        hasMatch = true;
+
+        break;
+    }
+  }
+
+  return hasMatch;
+}
+
+console.log(key_match({name: "Steve", age: 54}, {name: "Tamir", age: 54}));
