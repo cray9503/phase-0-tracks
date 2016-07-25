1151e0696d71558322be51934aeb3fc2f892afe3
Connor Ray <connor.ray.cb@gmail.com>
2016-07-25 17:23:51 -0600

    made the pizza disappear

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -3,3 +3,9 @@ console.log("Hello World!");
 var photo = document.getElementById("pizza");
 
 photo.style.border = "2px solid red";
+
+function pizzaFade(event) {
+  event.target.style.opacity = "0.5";
+}
+
+pizza.addEventListener("click", pizzaFade);
